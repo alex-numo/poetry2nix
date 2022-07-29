db2924efa50b3326f46eef0f7297e18fc1a5d0d9
alex-numo <alex@numo.com>
2022-07-29 00:31:49 -0400

    test removing develop

--- hooks/default.nix
+++ hooks/default.nix
@@ -1,40 +1,30 @@
-{ python
-, buildPackages
-, makeSetupHook
-, wheel
-, pip
-, pkgs
-}:
+{ python, buildPackages, makeSetupHook, wheel, pip, pkgs }:
 let
   callPackage = python.pythonForBuild.pkgs.callPackage;
   pythonInterpreter = python.pythonForBuild.interpreter;
   pythonSitePackages = python.sitePackages;
 
-  nonOverlayedPython = pkgs.python3.pythonForBuild.withPackages (ps: [ ps.tomlkit ]);
+  nonOverlayedPython =
+    pkgs.python3.pythonForBuild.withPackages (ps: [ ps.tomlkit ]);
   makeRemoveSpecialDependenciesHook = { fields, kind }:
-    nonOverlayedPython.pkgs.callPackage
-      (
-        {}:
-        makeSetupHook
-          {
-            name = "remove-path-dependencies.sh";
-            deps = [ ];
-            substitutions = {
-              # NOTE: We have to use a non-overlayed Python here because otherwise we run into an infinite recursion
-              # because building of tomlkit and its dependencies also use these hooks.
-              pythonPath = nonOverlayedPython.pkgs.makePythonPath [ nonOverlayedPython ];
-              pythonInterpreter = nonOverlayedPython.interpreter;
-              pyprojectPatchScript = "${./pyproject-without-special-deps.py}";
-              fields = fields;
-              kind = kind;
-            };
-          } ./remove-special-dependencies.sh
-      )
-      { };
-in
-{
+    nonOverlayedPython.pkgs.callPackage ({ }:
+      makeSetupHook {
+        name = "remove-path-dependencies.sh";
+        deps = [ ];
+        substitutions = {
+          # NOTE: We have to use a non-overlayed Python here because otherwise we run into an infinite recursion
+          # because building of tomlkit and its dependencies also use these hooks.
+          pythonPath =
+            nonOverlayedPython.pkgs.makePythonPath [ nonOverlayedPython ];
+          pythonInterpreter = nonOverlayedPython.interpreter;
+          pyprojectPatchScript = "${./pyproject-without-special-deps.py}";
+          fields = fields;
+          kind = kind;
+        };
+      } ./remove-special-dependencies.sh) { };
+in {
   removePathDependenciesHook = makeRemoveSpecialDependenciesHook {
-    fields = [ "path" ];
+    fields = [ "path" "develop" ];
     kind = "path";
   };
 
@@ -43,48 +33,29 @@ in
     kind = "git";
   };
 
+  pipBuildHook = callPackage ({ pip, wheel }:
+    makeSetupHook {
+      name = "pip-build-hook.sh";
+      deps = [ pip wheel ];
+      substitutions = { inherit pythonInterpreter pythonSitePackages; };
+    } ./pip-build-hook.sh) { };
 
-  pipBuildHook = callPackage
-    (
-      { pip, wheel }:
-      makeSetupHook
-        {
-          name = "pip-build-hook.sh";
-          deps = [ pip wheel ];
-          substitutions = {
-            inherit pythonInterpreter pythonSitePackages;
-          };
-        } ./pip-build-hook.sh
-    )
-    { };
-
-  poetry2nixFixupHook = callPackage
-    (
-      {}:
-      makeSetupHook
-        {
-          name = "fixup-hook.sh";
-          deps = [ ];
-          substitutions = {
-            inherit pythonSitePackages;
-            filenames = builtins.concatStringsSep " " [
-              "pyproject.toml"
-              "README.md"
-            ];
-          };
-        } ./fixup-hook.sh
-    )
-    { };
+  poetry2nixFixupHook = callPackage ({ }:
+    makeSetupHook {
+      name = "fixup-hook.sh";
+      deps = [ ];
+      substitutions = {
+        inherit pythonSitePackages;
+        filenames =
+          builtins.concatStringsSep " " [ "pyproject.toml" "README.md" ];
+      };
+    } ./fixup-hook.sh) { };
 
   # When the "wheel" package itself is a wheel the nixpkgs hook (which pulls in "wheel") leads to infinite recursion
   # It doesn't _really_ depend on wheel though, it just copies the wheel.
-  wheelUnpackHook = callPackage
-    ({}:
-      makeSetupHook
-        {
-          name = "wheel-unpack-hook.sh";
-          deps = [ ];
-        } ./wheel-unpack-hook.sh
-    )
-    { };
+  wheelUnpackHook = callPackage ({ }:
+    makeSetupHook {
+      name = "wheel-unpack-hook.sh";
+      deps = [ ];
+    } ./wheel-unpack-hook.sh) { };
 }
